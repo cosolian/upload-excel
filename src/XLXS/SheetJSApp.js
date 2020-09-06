e49e8a3c59d003ea317fe3caa3f943c0e80c7691
chayan duangsaeng <chayan.duangsaeng@scb.co.th>
2020-09-06 12:34:34 +0700

    add select column

--- src/XLXS/SheetJSApp.js
+++ src/XLXS/SheetJSApp.js
@@ -5,10 +5,17 @@ import DataInput from "./DataInput";
 import OutTable from "./OutTable";
 
 /* generate an array of column objects */
-const make_cols = refstr => {
+const make_cols = (refstr, columns) => {
+  console.log("colums ", columns)
+  const columnArray = columns.split(",");
+  //const columns = ["A", "M", "Z"]
   let o = [],
     C = XLSX.utils.decode_range(refstr).e.c + 1;
-  for (var i = 0; i < C; ++i) o[i] = { name: XLSX.utils.encode_col(i), key: i };
+  for (var i = 0; i < C; ++i) {
+    if (columnArray.find(column => column.toUpperCase() === XLSX.utils.encode_col(i))) {
+      o[i] = { name: XLSX.utils.encode_col(i), key: i };
+    }
+  };
   return o;
 };
 
@@ -17,12 +24,14 @@ class SheetJSApp extends React.Component {
     super(props);
     this.state = {
       data: [] /* Array of Arrays e.g. [["a","b"],[1,2]] */,
-      cols: [] /* Array of column objects e.g. { name: "C", K: 2 } */
+      cols: [] /* Array of column objects e.g. { name: "C", K: 2 } */,
+      value: ""
     };
     this.handleFile = this.handleFile.bind(this);
-    this.exportFile = this.exportFile.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    //this.exportFile = this.exportFile.bind(this);
   }
-  handleFile(file /*:File*/) {
+  handleFile(file) {
     /* Boilerplate to set up FileReader */
     const reader = new FileReader();
     const rABS = !!reader.readAsBinaryString;
@@ -36,25 +45,42 @@ class SheetJSApp extends React.Component {
       /* Convert array of arrays */
       const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
       /* Update state */
-      this.setState({ data: data, cols: make_cols(ws["!ref"]) });
+      this.setState({ data: data, cols: make_cols(ws["!ref"], this.state.value) });
     };
     if (rABS) reader.readAsBinaryString(file);
     else reader.readAsArrayBuffer(file);
   }
-  exportFile() {
-    /* convert state to workbook */
-    const ws = XLSX.utils.aoa_to_sheet(this.state.data);
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, "SheetJS");
-    /* generate XLSX file and send to client */
-    XLSX.writeFile(wb, "sheetjs.xlsx");
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
   }
+
+  // exportFile() {
+  //   /* convert state to workbook */
+  //   const ws = XLSX.utils.aoa_to_sheet(this.state.data);
+  //   const wb = XLSX.utils.book_new();
+  //   XLSX.utils.book_append_sheet(wb, ws, "SheetJS");
+  //   /* generate XLSX file and send to client */
+  //   XLSX.writeFile(wb, "sheetjs.xlsx");
+  // }
   render() {
     return (
       <DragDropFile handleFile={this.handleFile}>
         <div className="row">
           <div className="col-xs-12">
-            <DataInput handleFile={this.handleFile} />
+            <table>
+              <tr>
+                <td>
+                  <label>
+                    Columns:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                  </label>
+                </td>
+                <td>
+                  <DataInput handleFile={this.handleFile} />
+                </td>
+              </tr>
+            </table>
           </div>
         </div>
         {/* <div className="row">
